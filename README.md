4e52b934a2af7aedfdffda602fac7a831ea27b34
Unim8trix <65271507+Unim8trix@users.noreply.github.com>
2026-02-14 13:52:36 +0100

    First release

--- README.md
+++ README.md
@@ -1,2 +1,172 @@
-# GnomeBook
-Arch Linux on HP Elitebook 845 G10 using Gnome Desktop Environment
+<h1 align="center">
+   <img src="./.github/assets/arch_logo.png" width="200px" />
+   <br>
+      Arch Linux on HP Elitebook 845 G10
+   <br>
+      <img src="./.github/assets/nord_palet.png" width="600px" /> <br>
+
+   <div align="center">
+      <p></p>
+      <div align="center">
+         <a href="https://github.com/Unim8trix/GnomeBook/">
+            <img src="https://img.shields.io/github/repo-size/Unim8trix/GnomeBook?color=B16286&labelColor=282828&style=for-the-badge&logo=github&logoColor=B16286">
+         </a>
+         <a = href="https://archlinux.org">
+            <img src="https://img.shields.io/badge/Arch_Linux-1793D1?style=for-the-badge&labelColor=282828&logo=arch-linux&logoColor=458588&color=458588">
+         </a>
+         <a href="https://github.com/Unim8trix/GnomeBook/blob/main/LICENSE">
+            <img src="https://img.shields.io/static/v1.svg?style=for-the-badge&label=License&message=MIT&colorA=282828&colorB=98971A&logo=unlicense&logoColor=98971A&"/>
+         </a>
+      </div>
+      <br>
+   </div>
+</h1>
+
+
+### 🖼️ Gallery
+
+<p align="center">
+   <img src="./.github/assets/screenshot.png" style="margin-bottom: 10px;"/> <br>
+   Screenshot last updated <b>xx.2026</b>
+</p>
+
+# 🗃️ Overview
+
+This scripts setup Archlinux on a HP Elitebook 845 G10 with Gnome as Desktop Environment.
+
+Iam using full disk encryption, BTRFS as filesystem and secure boot.
+
+Main usage is Cloud Development, Virtualization and some gaming.
+
+Localisations are for german, so adjust for your own needs.
+
+A big THANKs to [SOL](https://github.com/SolDoesTech/HyprV2) for getting me into hyprland!
+
+## 📚 Layout
+
+-   [install.sh](install.sh) 📜 Shell script for set up partitons, filesystems and first pacstrap
+-   [chroot.sh](chroot.sh) 📜 Shell script for the rest of the system
+-   [config](config) ⚙️ configuration files
+-   [fonts](fonts/) ✒️ terminal nerd fonts
+-   [wallpapers](wallpapers/) 🌄 wallpapers collection
+
+## 📦 List of installed Packages
+
+| Arch packages                          | Description                                                                                   |
+| :--------------------------------------| :---------------------------------------------------------------------------------------------|
+| **mesa, vulkan-radeon**                | OpenGL and Vulkan drivers for amd gpu |
+| **gnome**                              | Gnome Desktop Environment |
+| **firefox-developer-edition-i18n-de**  | My default browser |
+| **nano**                               | Terminal editor |
+| **mc, unzip**                          | Midnight Commander, Norton Commander clone for console |
+| **mission-center**                     | Resource monitor |
+| **zfz, jq, yq**                        | some usefull cmdline utils |
+| **noto-fonts, noto-fonts-emoji**       | Default fonts/emoji fonts |
+| **pipewire**                           | Audioserver |
+| **starship**                           | Customize terminal shell |
+| **fastfetch**                          | CLI based system information |
+| **papirus-icon-theme**                 | Icon theme |
+
+
+# 🚀 Installation
+
+> [!CAUTION]
+> Applying custom configurations, especially those related to your operating system, can have unexpected consequences and may interfere with your system's normal behavior. While I have tested these configurations on my own setup, there is no guarantee that they will work flawlessly for you.
+> **I am not responsible for any issues that may arise from using this configuration.**
+
+> [!NOTE]
+> This configuration is build for the HP Elitebook 845 G10.
+>If you want to use it with your hardware you to review the configuration contents and make necessary modifications to customize it to your needs before attempting the installation!
+
+## Boot arch linux live image
+
+Get the latest live image from [Arch Linux Website](https://archlinux.org/download/) and make a bootable USB stick. Boot the
+elitebook with the USB stick.
+
+```bash
+loadkeys de-latin1-nodeadkeys
+iwctl
+# station wlan0 connect "YOUR_WLAN_SSID"
+timedatectl set-timezone Europe/Berlin
+timedatectl set-ntp true
+```
+
+## Clone this repo and start Installation
+
+```bash
+pacman -Sy git
+git clone https://github.com/Unim8trix/GnomeBook
+cd GnomeBook
+./install.sh
+```
+
+# 📝 Manual Config
+
+## Secure Boot
+
+First, go into BIOS Settings: Secure Boot, enable **Clear Keys**, save and reboot.
+
+Install sbctl
+
+```bash
+yay -Sy sbctl
+```
+
+Create keys (i dont include microsoft ca)
+
+```bash
+sudo sbctl create-keys
+```
+
+Check if secure boot is in setup mode, if not reboot and enable "Clear keys..." in BIOS
+
+```bash
+sudo sbctl status
+```
+
+Enroll keys and sign
+
+```bash
+sudo sbctl enroll-keys
+
+sudo sbctl sign -s /boot/vmlinuz-linux
+sudo sbctl sign -s /boot/EFI/BOOT/BOOTX64.EFI
+sudo sbctl sign -s /boot/EFI/systemd/systemd-bootx64.efi
+
+# enable automatic signing using pacman hook
+sudo systemctl enable --now systemd-boot-update.service
+
+# check
+sudo sbctl verify
+```
+
+Now reboot,  boot with secure bios would be automaticly enabled (check with `sudo sbctl status`)
+
+## Gaming
+
+Enable 32-Bit Lib
+
+Comment out `[multilib]` section in `/etc/pacman.conf`
+
+```bash
+[multilib]
+Include = /etc/pacman.d/mirrorlist
+```
+
+Install Vulkan Driver and Libs
+
+```bash
+yay -Sy lib32-mesa lib32-vulkan-radeon lib32-vulkan-icd-loader lib32-systemd ttf-liberation
+```
+
+Reboot your book and start installing the steam client
+
+
+```bash
+yay -Sy steam
+```
+
+
+<div align="right">
+  <a href="#readme">Back to the Top</a>
+</div>
---
+++ install.sh
@@ -0,0 +1,172 @@
+#!/usr/bin/sh
+
+init() {
+    # Vars
+    USERNAME='me'
+    HOSTNAME='hadal'
+
+    # Colors
+    NORMAL=$(tput sgr0)
+    YELLOW=$(tput setaf 7)
+    BLACK=$(tput setaf 0)
+    RED=$(tput setaf 1)
+    GREEN=$(tput setaf 2)
+    YELLOW=$(tput setaf 3)
+    BLUE=$(tput setaf 4)
+    MAGENTA=$(tput setaf 5)
+    CYAN=$(tput setaf 6)
+    BRIGHT=$(tput bold)
+    UNDERLINE=$(tput smul)
+}
+
+confirm() {
+    echo -en "[${GREEN}y${NORMAL}/${RED}n${NORMAL}]: "
+    read -n 1 -r
+    echo
+    if [[ ! $REPLY =~ ^[Yy]$ ]]
+    then
+        exit 0
+    fi
+}
+
+print_header() {
+    clear
+    echo -E "$BRIGHT
+     ____ ___      .__           ______  __         .__
+    |    |   \____ |__| _____   /  __  \/  |________|__|__  ___
+    |    |   /    \|  |/     \  >      <   __\_  __ \  \  \/  /
+    |    |  /   |  \  |  Y Y  \/   --   \  |  |  | \/  |>    <
+    |______/|___|__/__|__|_|__/\________/__|  |__|  |__/__/\__\
+
+                $WHITE https://github.com/Unim8trix $NORMAL
+"
+}
+
+install() {
+    echo -e "\n${YELLOW}Phase 1: setting up disk structure and install base system${NORMAL}\n"
+    echo -en "\n${RED}The NVMEN1 Disk will now be WIPED and all data will be destroyed! Are your sure to continue?${NORMAL}\n"
+    confirm
+
+    echo -e "${YELLOW}Erase existing luks partition NVME0n1p3${NORMAL}\n"
+    cryptsetup erase /dev/nvme0n1p3
+    echo -e "${GREEN}Done${NORMAL}\n"
+    sleep 0.5
+
+    echo -e "${YELLOW}Wipe and delete old partition layout${NORMAL}\n"
+    wipefs -af /dev/nvme0n1
+    sgdisk --zap-all --clear /dev/nvme0n1
+    partprobe /dev/nvme0n1
+    echo -e "${GREEN}Done${NORMAL}\n"
+    sleep 0.2
+
+    echo -e "${YELLOW}Create new disk layout${NORMAL}\n"
+    sgdisk -n 0:0:+1024MiB -t 0:ef00 -c 0:esp /dev/nvme0n1
+    sgdisk -n 0:0:+65536MiB -t 0:8200 -c 0:swap /dev/nvme0n1
+    sgdisk -n 0:0:0 -t 0:8309 -c 0:luks /dev/nvme0n1
+    partprobe /dev/nvme0n1
+    echo -e "${GREEN}Done${NORMAL}\n"
+    sleep 0.2
+
+    echo -e "${YELLOW}Format EFI partition${NORMAL}\n"
+    mkfs.vfat -F 32 -n ESP /dev/nvme0n1p1
+    echo -e "${GREEN}Done${NORMAL}\n"
+    sleep 0.2
+
+    echo -e "${YELLOW}Create crypted partition for root${NORMAL}\n"
+    cryptsetup luksFormat /dev/nvme0n1p3
+    echo -e "${YELLOW}Open luks partion, type in your new password${NORMAL}\n"
+    cryptsetup open /dev/nvme0n1p3 cryptsys
+    echo -e "${GREEN}Done${NORMAL}\n"
+    sleep 0.5
+
+    echo -e "${YELLOW}Create BTRFS filesystem and subvolumes${NORMAL}\n"
+    mkfs.btrfs -L ARCH -f /dev/mapper/cryptsys
+    mount /dev/mapper/cryptsys /mnt
+    btrfs sub create /mnt/@
+    btrfs sub create /mnt/@home
+    btrfs sub create /mnt/@cache
+    btrfs sub create /mnt/@log
+    btrfs sub create /mnt/@tmp
+    btrfs sub create /mnt/@pkg
+    sleep 1
+    umount /mnt
+    echo -e "${GREEN}Done${NORMAL}\n"
+    sleep 0.5
+
+    echo -e "${YELLOW}Remount root volume with btrfs subvolume options${NORMAL}\n"
+    mount -o noatime,space_cache=v2,discard=async,compress=zstd:1,subvol=@ /dev/mapper/cryptsys /mnt
+    mount --mkdir -o noatime,space_cache=v2,discard=async,compress=zstd:1,subvol=@home /dev/mapper/cryptsys /mnt/home
+    mount --mkdir -o noatime,space_cache=v2,discard=async,compress=zstd:1,subvol=@log /dev/mapper/cryptsys /mnt/var/log
+    mount --mkdir -o noatime,space_cache=v2,discard=async,compress=zstd:1,subvol=@tmp /dev/mapper/cryptsys /mnt/var/tmp
+    mount --mkdir -o noatime,space_cache=v2,discard=async,compress=zstd:1,subvol=@cache /dev/mapper/cryptsys /mnt/var/cache
+    mkdir -p /mnt/var/cache/pacman/pkg
+    mount -o noatime,space_cache=v2,discard=async,compress=zstd:1,subvol=@pkg /dev/mapper/cryptsys /mnt/var/cache/pacman/pkg
+    mount --mkdir /dev/nvme0n1p1 /mnt/boot
+
+    echo -e "${GREEN}Done${NORMAL}\n"
+    sleep 5
+    clear
+
+    echo -e "${YELLOW}Install minimal base system with pacstrap${NORMAL}\n"
+    sleep 2
+    pacstrap /mnt base base-devel linux-zen linux-firmware btrfs-progs amd-ucode networkmanager zsh git-lfs curl wget man-db mlocate reflector nano-syntax-highlighting
+    echo -e "${GREEN}Done${NORMAL}\n"
+    sleep 2
+
+    echo -e "${YELLOW}Generate filesystem table${NORMAL}\n"
+    genfstab -p -L /mnt >> /mnt/etc/fstab
+    echo -e "${GREEN}Done${NORMAL}\n"
+    sleep 0.5
+
+    echo -e "${YELLOW}Copy zsh profiles to /mnt/root${NORMAL}\n"
+    cp /etc/zsh/zprofile /mnt/root/.zprofile
+    cp /etc/zsh/zshrc /mnt/root/.zshrc
+    cp /root/GnomeBook/chroot.sh /mnt/root && chmod +x /mnt/root/chroot.sh
+    echo -e "${YELLOW}Set you new root password${NORMAL}\n"
+    arch-chroot /mnt /bin/passwd root
+    sleep 0.5
+    arch-chroot /mnt /bin/chsh -s /bin/zsh
+    echo -e "${GREEN}Done${NORMAL}\n"
+    sleep 0.5
+
+    echo -e "${YELLOW}Chroot into new system and create initial settings${NORMAL}\n"
+    sleep 3
+    arch-chroot /mnt /bin/zsh -c "/root/chroot.sh"
+    echo -e "${GREEN}Done${NORMAL}\n"
+    echo -e "${GREEN}Back in normal environment${NORMAL}\n"
+    sleep 3
+
+    echo -e "${YELLOW}Copy dotfiles${NORMAL}\n"
+    cp -R /root/GnomeBook/config/* /mnt/home/${USERNAME}/.config/
+    arch-chroot /mnt sudo chown -R ${USERNAME}:${USERNAME} /home/${USERNAME}/.config
+    echo -e "${GREEN}Done${NORMAL}\n"
+    sleep 0.5
+
+    echo -e "${YELLOW}Copy fonts${NORMAL}\n"
+    cp -R /root/GnomeBook/fonts/* /mnt/home/${USERNAME}/.local/share/fonts/
+    arch-chroot /mnt sudo chown -R ${USERNAME}:${USERNAME} /home/${USERNAME}/.local/share/fonts
+    echo -e "${GREEN}Done${NORMAL}\n"
+    sleep 0.5
+
+    echo -e "${YELLOW}Copy wallpapers${NORMAL}\n"
+    cp -R /root/GnomeBook/wallpaper/* /mnt/home/${USERNAME}/Bilder/Wallpaper/
+    arch-chroot /mnt sudo chown -R ${USERNAME}:${USERNAME} /home/${USERNAME}/Bilder
+    echo -e "${GREEN}Done${NORMAL}\n"
+    sleep 0.5
+
+    echo -e "${YELLOW}Unmounting filesystems${NORMAL}\n"
+    rm /mnt/root/chroot.sh
+    rm -rf /mnt/home/${USERNAME}/yay
+    umount -R /mnt
+    echo -e "${GREEN}Finished${NORMAL}\n"
+    echo -e "${YELLOW}System can now be rebooted${NORMAL}\n"
+}
+
+main() {
+    init
+    print_header
+
+    install
+}
+
+main && exit 0
