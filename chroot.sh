4c82a0fe51baf9a4b9f5aac384088cdac93e352d
Unim8trix <65271507+Unim8trix@users.noreply.github.com>
2026-02-15 09:53:59 +0100

    Add extension-manager

--- chroot.sh
+++ chroot.sh
@@ -157,7 +157,7 @@ install() {
     echo -e "${YELLOW}Install Gnome and tools${NORMAL}\n"
     sleep 2
     su ${USERNAME} -c "yay --noconfirm -Sy gnome firefox-developer-edition-i18n-de \
-      plymouth-theme-arch-charge gnome-shell-extension-manager power-profiles-daemon \
+      plymouth-theme-arch-charge extension-manager power-profiles-daemon \
       gnome-tweaks papirus-icon-theme"
     echo -e "${GREEN}Done${NORMAL}\n"
     sleep 5
